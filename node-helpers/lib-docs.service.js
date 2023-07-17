d4348dc55fda391f803097058ab7a30016b8368a
Sunil Kumar <sunil.divyam@gmail.com>
2023-07-17 16:57:31 +0530

    updates

--- node-helpers/lib-docs.service.js
+++ node-helpers/lib-docs.service.js
@@ -1,6 +1,17 @@
 const { readFileSync, writeFileSync } = require('./file.service');
 const showdown = require('showdown');
 
+const developmentUrl = 'http://localhost:4200';
+const stagingUrl = 'https://docs-annu-business.web.app';
+const productionUrl = 'https://ngx-libs.annuadvent.com';
+const staticRoutes = [
+    '/tnc/terms-and-conditions',
+    '/tnc/privacy-policy',
+    '/contact-us',
+    '/about-us',
+    '/libs',
+]
+
 const md2html = (mdText) => {
     const converter = new showdown.Converter();
     let html = converter.makeHtml(mdText);
@@ -161,7 +172,147 @@ const createLibDocsImports = () => {
     );
 };
 
+const createSitemapXml = (libNames, sitemapEnv, sitemapEnvUrl) => {
+    const lastMod = `<lastmod>${(new Date(Date.now())).toISOString()}</lastmod>`;
+    const priority = (pr) => `<priority>${pr}</priority>`;
+
+    let xmlStr = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    <url>
+        <loc>${sitemapEnvUrl}</loc>
+        ${lastMod}
+        ${priority('1.00')}
+    </url>
+    ${staticRoutes.map(rt => `<url>
+        <loc>${sitemapEnvUrl}${rt}</loc>
+        ${lastMod}
+        ${priority('0.80')}
+    </url>
+    `).join('')}`;
+
+
+
+    libNames.forEach(libName => {
+        // Lib route
+        xmlStr += `<url>
+        <loc>${sitemapEnvUrl}/libs/${libName}</loc>
+        ${lastMod}
+        ${priority('0.80')}
+    </url>
+    `;
+
+        // read projects/ngx-ssr-libdocs-app/src/assets/[libNaame]/documentation.json
+        const url = `projects/ngx-ssr-libdocs-app/src/assets/${libName}/documentation.json`;
+        const libJsonStr = readFileSync(url);
+        const libJson = JSON.parse(libJsonStr);
+        const assets = {
+            components: libJson.components,
+            classes: libJson.classes,
+            interfaces: libJson.interfaces,
+            directives: libJson.directives,
+            guards: libJson.injectables.filter(inj => inj.file.includes('.guard.')),
+            interceptors: libJson.injectables.filter(inj => inj.file.includes('.interceptor.')),
+            services: libJson.injectables.filter(inj => !inj.file.includes('.guard.') && !inj.file.includes('.interceptor.')),
+        };
+
+
+        Object.keys(assets).forEach(assetType => {
+            // AssetType route
+            xmlStr += `<url>
+        <loc>${sitemapEnvUrl}/libs/${libName}/${assetType}</loc>
+        ${lastMod}
+        ${priority('0.80')}
+    </url>
+    `;
+
+            assets[assetType].forEach(ast => xmlStr += `<url>
+        <loc>${sitemapEnvUrl}/libs/${libName}/${assetType}/${ast.name}</loc>
+        ${lastMod}
+        ${priority('0.80')}
+    </url>
+    `)
+        });
+    });
+
+    xmlStr += `
+</urlset>`;
+
+    const sitemapWriteUrl = `projects/ngx-ssr-libdocs-app/src/sitemaps/${sitemapEnv}/sitemap.xml`;
+    writeFileSync(sitemapWriteUrl, xmlStr, true);
+
+    console.log(
+        'GENERATED - OK \n',
+        sitemapWriteUrl + '\n',
+    );
+};
+
+const createSitemapXmls = () => {
+    const libNames = process.argv.slice(4);
+
+    if (!libNames || !libNames.length) throw new Error('Array of lib names required.');
+
+    createSitemapXml(libNames, 'development', developmentUrl);
+    createSitemapXml(libNames, 'staging', stagingUrl);
+    createSitemapXml(libNames, 'production', productionUrl);
+};
+
+const createPrerenderLibsRoutes = (libNames) => {
+    let routesStr = `/
+${staticRoutes.map(rt => `${rt}
+`).join('')}`;
+
+
+    libNames.forEach(libName => {
+        // Lib route
+        routesStr += `/libs/${libName}
+`;
+
+        // read projects/ngx-ssr-libdocs-app/src/assets/[libNaame]/documentation.json
+        const url = `projects/ngx-ssr-libdocs-app/src/assets/${libName}/documentation.json`;
+        const libJsonStr = readFileSync(url);
+        const libJson = JSON.parse(libJsonStr);
+        const assets = {
+            components: libJson.components,
+            classes: libJson.classes,
+            interfaces: libJson.interfaces,
+            directives: libJson.directives,
+            guards: libJson.injectables.filter(inj => inj.file.includes('.guard.')),
+            interceptors: libJson.injectables.filter(inj => inj.file.includes('.interceptor.')),
+            services: libJson.injectables.filter(inj => !inj.file.includes('.guard.') && !inj.file.includes('.interceptor.')),
+        };
+
+
+        Object.keys(assets).forEach(assetType => {
+            // AssetType route
+            routesStr += `/libs/${libName}/${assetType}
+`;
+
+            assets[assetType].forEach(ast => routesStr += `/libs/${libName}/${assetType}/${ast.name}
+`)
+        });
+    });
+
+    const prerenderRoutesWriteUrl = `projects/ngx-ssr-libdocs-app/src/prerender-routes/routes.txt`;
+    writeFileSync(prerenderRoutesWriteUrl, routesStr, true);
+
+    console.log(
+        'GENERATED - OK \n',
+        prerenderRoutesWriteUrl + '\n',
+    );
+};
+
+const createPrerenderRoutes = () => {
+    const libNames = process.argv.slice(4);
+
+    if (!libNames || !libNames.length) throw new Error('Array of lib names required.');
+
+    createPrerenderLibsRoutes(libNames);
+}
+
 module.exports = {
     createAllLibDocsJson,   // generates libs-info.json, having all lif infos.
     createLibDocsImports,   // generates all lib resources cmp/cmpdata/svc imports for dynamic render.
+    createSitemapXmls,      // generates sitemap.xml for all resources of all libs
+    createPrerenderRoutes,  // generates routes.txt for all environments and for all libs, to prerender routes
 }
